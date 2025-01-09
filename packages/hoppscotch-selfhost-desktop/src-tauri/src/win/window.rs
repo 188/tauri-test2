f1d7a092302a3344f515a5fb1ac74b1f028b2a41
hantianwei <hantianwei@gmail.com>
2025-01-09 19:42:14 +0800

    123

--- packages/hoppscotch-selfhost-desktop/src-tauri/src/win/window.rs
+++ packages/hoppscotch-selfhost-desktop/src-tauri/src/win/window.rs
@@ -1,12 +1,8 @@
 use hex_color::HexColor;
 use std::mem::transmute;
-use std::sync::atomic::{AtomicUsize, Ordering};
 use std::{ffi::c_void, mem::size_of, ptr};
-use tauri::Emitter;
 use tauri::Listener;
-use tauri::WebviewWindow;
 use tauri::{App, Manager};
-use windows::Win32::UI::WindowsAndMessaging::HWND;
 
 use windows::Win32::UI::Controls::{
     WTA_NONCLIENT, WTNCA_NODRAWICON, WTNCA_NOMIRRORHELP, WTNCA_NOSYSMENU,
@@ -23,8 +19,12 @@ use windows::Win32::UI::Controls::WTNCA_NODRAWCAPTION;
 
 use winver::WindowsVersion;
 
+struct SendableHWND(HWND);
+
+unsafe impl Send for SendableHWND {}
+unsafe impl Sync for SendableHWND {}
+
 fn hex_color_to_colorref(color: HexColor) -> COLORREF {
-    // TODO: Remove this unsafe, This operation doesn't need to be unsafe!
     unsafe { COLORREF(transmute::<[u8; 4], u32>([color.r, color.g, color.b, 0])) }
 }
 
@@ -34,14 +34,14 @@ struct WinThemeAttribute {
 }
 
 #[cfg(target_os = "windows")]
-fn update_bg_color(hwnd: &HWND, bg_color: HexColor) {
+fn update_bg_color(hwnd: &SendableHWND, bg_color: HexColor) {
     let use_dark_mode = BOOL::from(true);
 
     let final_color = hex_color_to_colorref(bg_color);
 
     unsafe {
         DwmSetWindowAttribute(
-            HWND(hwnd.0),
+            hwnd.0,
             DWMWA_USE_IMMERSIVE_DARK_MODE,
             ptr::addr_of!(use_dark_mode) as *const c_void,
             size_of::<BOOL>().try_into().unwrap(),
@@ -53,7 +53,7 @@ fn update_bg_color(hwnd: &HWND, bg_color: HexColor) {
     if version >= WindowsVersion::new(10, 0, 22000) {
         unsafe {
             DwmSetWindowAttribute(
-                HWND(hwnd.0),
+                hwnd.0,
                 DWMWA_CAPTION_COLOR,
                 ptr::addr_of!(final_color) as *const c_void,
                 size_of::<COLORREF>().try_into().unwrap(),
@@ -68,7 +68,7 @@ fn update_bg_color(hwnd: &HWND, bg_color: HexColor) {
 
     unsafe {
         SetWindowThemeAttribute(
-            HWND(hwnd.0),
+            hwnd.0,
             WTA_NONCLIENT,
             ptr::addr_of!(options) as *const c_void,
             size_of::<WinThemeAttribute>().try_into().unwrap(),
@@ -82,26 +82,15 @@ pub fn setup_win_window(app: &mut App) {
     let window = app.get_webview_window("main").unwrap();
     let win_handle = window.hwnd().unwrap();
 
-    // 使用 AtomicUsize 来存储 HWND 的值
-    let win_handle = AtomicUsize::new(win_handle.0 as usize);
-
-    let win_clone = win_handle.clone();
+    let sendable_hwnd = SendableHWND(win_handle.clone());
 
     app.listen_any("hopp-bg-changed", move |ev| {
         let payload = serde_json::from_str::<&str>(ev.payload()).unwrap().trim();
 
-        let color = HexColor::parse_rgb(payload).unwrap();
+        let color = HexColor::parse_rgb(payload).unwrap_or_else(|_| HexColor::rgb(23, 23, 23));
 
-        // 获取 HWND 的值
-        let hwnd_value = win_clone.load(Ordering::SeqCst);
-        let hwnd = HWND(hwnd_value as isize);
-
-        update_bg_color(&hwnd, color);
+        update_bg_color(&sendable_hwnd, color);
     });
 
-    // 获取 HWND 的值
-    let hwnd_value = win_handle.load(Ordering::SeqCst);
-    let hwnd = HWND(hwnd_value as isize);
-
-    update_bg_color(&hwnd, HexColor::rgb(23, 23, 23));
+    update_bg_color(&SendableHWND(win_handle), HexColor::rgb(23, 23, 23));
 }
